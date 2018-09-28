2b275d6a3110a9031f7db72d4aa0b518bc490a86
max <1223max@gmail.com>
2018-09-28 13:50:20 +0200

    walking

---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+    public float playerSpeed;  //allows us to be able to change speed in Unity
+    public Vector2 jumpHeight;
+    public bool isGrounded = false;
+
+    void Update ()
+    {
+      Debug.Log(isGrounded);
+    // transform.Translate(playerSpeed * Time.deltaTime, 0f, 0f);  //makes player run
+
+    if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))  
+        {
+            if(isGrounded)
+             {
+                GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
+                isGrounded = false;
+            }
+             
+            
+        }
+
+    }
+   
+
+    void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if (collision.collider.tag == "Ground") 
+        {
+                         
+            isGrounded = true;
+                        
+        }
+    }
+
+}
